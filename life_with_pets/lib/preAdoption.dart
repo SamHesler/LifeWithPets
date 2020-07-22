2c0de9c7685f5c1f629587369f032c9d52459299
SamHesler <heslersamant@seattleu.edu>
2020-07-21 17:18:46 -0700

    Home Screen Created
    A scrollable home screen was set up that has buttons corresponding to the 4 main sections of the app.

---
+++ life_with_pets/lib/preAdoption.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PreAdoption extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Pre-Adoption Information')),
+        body: Text('Here we are'));
+  }
+}
